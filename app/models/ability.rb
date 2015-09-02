0c091db0660829c26432d2d507d5518d8719a8d4
kjj6198 <kjj6198@gmail.com>
2015-09-02 16:53:32 +0800

    fix the stytax error

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,18 +2,9 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
-    if user.has_role?(:admin)
-      can :all
-    end
-	else
-	  basic_edit_menu
-	end
+    
   end
   
 
-  def basic_edit_menu
-  	can :manege, Menu do |menu|
-  	  menu.user_id == user.id
-  	end
-  end
+  
 end
