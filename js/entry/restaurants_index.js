69582b4653ebbde491d7762a9293370779ba52cc
abalone0204 <abalone0204@gmail.com>
2015-09-02 12:09:58 +0800

    done add webpack

---
+++ js/entry/restaurants_index.js
@@ -0,0 +1,3 @@
+console.log('test');
+
+console.log('sreresrerestaurant');
---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+var path = require("path");
+var webpack = require("webpack");
+module.exports = {
+  context: __dirname,
+  entry: {
+    restaurants_index: './js/entry/restaurants_index.js'
+  },
+  output: {
+    path: path.join(__dirname, 'app', 'assets', 'javascripts', 'bundle'),
+    filename: '[name]_bundle.js',
+    publicPath: '/js/'
+  },
+  plugins: [
+    new webpack.ResolverPlugin(
+      new webpack.ResolverPlugin.DirectoryDescriptionFilePlugin("bower.json", ["main"])
+    )
+  ]
+};
