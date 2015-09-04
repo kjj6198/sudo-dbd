059befa3615aeafe96b5692674e2ee56e16b41a2
abalone0204 <abalone0204@gmail.com>
2015-09-05 02:20:02 +0800

    add destroy to orders

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,15 +1,24 @@
 class OrdersController < ApplicationController
-
+ before_action :find_menu
  def create
-   @order = Menu.find(params[:menu_id]).orders.create(order_params)
-   @menu = Menu.find(params[:menu_id])
-   
+   @order = @menu.orders.create(order_params)
    redirect_to menu_path(@menu), :notice => '成功新增訂餐！請記得繳費'
  end
 
+ def destroy
+     @order = Order.find(params[:id])
+     @order.destroy
+     redirect_to @menu
+ end
+
  private 
+ 
  def order_params
      params[:order].permit(:name,:food_name,:price, :has_paid, :note)
+ end
+
+ def find_menu
+     @menu = Menu.find(params[:menu_id])
  end
 
 end
