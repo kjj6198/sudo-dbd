1cd2065661c03c4a5baab14179569c88f4da2886
abalone0204 <abalone0204@gmail.com>
2015-09-04 21:41:44 +0800

    just for fun

---
+++ js/components/fancy_shrimp.js
@@ -0,0 +1,37 @@
+var Rx = require('rx');
+var animateTime = 2000;
+
+var $shrimp = $("#shrimp");
+var $sdbdText = $("#sdbd_text");
+var shrimpWidth = $shrimp.width();
+var shrimpOriginLeft = $shrimp.css("left");
+var shrimpClicks = Rx.Observable.fromEvent($shrimp, "click");
+
+shrimpClicks.
+throttle(animateTime).
+forEach(function(click) {
+    fancyShrimp();
+})
+// animateFunction
+var fancyShrimp = function() {
+    console.log('do');
+    flashAnimate($sdbdText);
+    cycleMove($shrimp)
+}
+var flashAnimate = function(elem) {
+    elem.css("opacity", 0);
+    elem.animate({
+        opacity: 1
+    }, animateTime);
+}
+var cycleMove = function(elem) {
+    var offsetLeft = elem.offset().left;
+    elem.animate({
+        left: -1 * offsetLeft
+    }, 250, function() {
+        elem.css("left", "120%");
+        elem.animate({
+            left: shrimpOriginLeft
+        }, 250);
+    })
+}
---
+++ js/entry/landing_page.js
@@ -0,0 +1,1 @@
+require('../components/fancy_shrimp.js');
