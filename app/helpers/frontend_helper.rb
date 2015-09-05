4240ed9d0a0129200245d23c04152343bcce2420
abalone0204 <abalone0204@gmail.com>
2015-09-05 21:01:41 +0800

    add time helper

--- app/helpers/frontend_helper.rb
+++ app/helpers/frontend_helper.rb
@@ -2,4 +2,20 @@ module FrontendHelper
     def show_navbar?
         params[:controller] == 'frontend'
     end
+
+    def render_time(datetime)
+        datetime.strftime("%I:%M %p")
+    end
+
+    def duration_time(endtime)
+        curTime = (endtime-DateTime.now).to_i
+        if curTime <= 0 
+            "已截止"
+        else
+            minutes = (curTime /60).to_i
+            seconds = curTime % 60
+            "#{minutes} : #{seconds}"
+        end
+        
+    end
 end
