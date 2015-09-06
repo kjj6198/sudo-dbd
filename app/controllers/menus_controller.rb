316bea2cd444fde1439977fe17f6d9b6d9c3f2c6
abalone0204 <abalone0204@gmail.com>
2015-09-06 16:39:00 +0800

    add login recommendation to menus#show

--- app/controllers/menus_controller.rb
+++ app/controllers/menus_controller.rb
@@ -1,5 +1,5 @@
 class MenusController < ApplicationController
-  before_action :auth_google_user!, except: [:index]
+  before_action :auth_google_user!, except: [:index, :show]
   before_action :find_menu, only: [:show, :bill]
   def index
     @menus = Menu.includes(:user, :restaurant).order(end_time: :asc, start_time: :desc);
