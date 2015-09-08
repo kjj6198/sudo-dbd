48b41850a3815615b292b0c9e58d4e3ad5aab20b
abalone0204 <abalone0204@gmail.com>
2015-09-08 11:26:59 +0800

    remove sidekiq

--- app/workers/dbd_worker.rb
+++ app/workers/dbd_worker.rb
@@ -1,8 +1,8 @@
 require "slack_outhook"
 
 class DbdWorker
-  include Sidekiq::Worker
-  include SlackOutHook
+  # include Sidekiq::Worker
+  # include SlackOutHook
   
   def self.perform_countdown(id, remain_time)
     puts "id is : #{id}" 
